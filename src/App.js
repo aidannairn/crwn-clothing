9927aecb62d1f75d69166e3b6234d0df19f1170f
aidannairn <aidan.nairn95@gmail.com>
2019-08-09 12:39:29 +1200

    Create Homepage component and connect to Sass styles.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import './App.css'
 
+import Homepage from './Homepage'
+
 function App() {
-  return <div>Hello World!</div>
+  return (
+    <div>
+      <Homepage />
+    </div>
+  )
 }
 
 export default App
