02cea9082f5f455ff2a15386ce3db0663e9e55e6
aidannairn <aidan.nairn95@gmail.com>
2019-08-11 17:16:23 +1200

    Display cart items on Checkout page.

--- src/components/cart-item/CartItem.jsx
+++ src/components/cart-item/CartItem.jsx
@@ -9,7 +9,7 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
       <div className="item-details">
         <span className="name">{name}</span>
         <span className="price">
-          {quantity} x ${price}.00
+          {quantity} x ${price}
         </span>
       </div>
     </div>
