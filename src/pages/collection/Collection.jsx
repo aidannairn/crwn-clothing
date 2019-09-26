e7f920fdd4ed431ede0d781d5cd3f428e2c6529b
aidannairn <aidan.nairn95@gmail.com>
2019-09-26 13:36:47 +1200

    Create collection selector based on url params.

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-import CollectionItem from '../../components/collection-item/CollectionItem'
+import { selectCollection } from '../../redux/shop/shop.selectors'
 import './collection.styles.scss'
 
-const Collection = ({ match }) => {
+const Collection = ({ collection }) => {
   return (
     <div className="catergory">
       <h2>COLLECTION PAGE</h2>
@@ -11,4 +12,8 @@ const Collection = ({ match }) => {
   )
 }
 
-export default Collection
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(Collection)
