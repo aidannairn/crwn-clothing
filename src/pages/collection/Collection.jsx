1133d4d718cdfe33fe9c2fff2618ff19dbc2ea97
aidannairn <aidan.nairn95@gmail.com>
2019-09-26 13:19:40 +1200

    Add collection route to Shop page.

---
+++ src/pages/collection/Collection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import CollectionItem from '../../components/collection-item/CollectionItem'
+import './collection.styles.scss'
+
+const Collection = ({ match }) => {
+  return (
+    <div className="catergory">
+      <h2>COLLECTION PAGE</h2>
+    </div>
+  )
+}
+
+export default Collection
